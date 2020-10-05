7db4ca0aa3b617c1480fcba1432ba31731b48028
Ethan Green <egreen4@unca.edu>
2020-10-05 17:02:57 -0400

    Maven project init

---
+++ src/main/java/edu/unca/csci/App.java
@@ -0,0 +1,13 @@
+package edu.unca.csci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
