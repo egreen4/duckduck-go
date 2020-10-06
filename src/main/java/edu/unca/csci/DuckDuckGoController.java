29b3f990f375efffa5b672e72b958608c4214148
Ethan Green <egreen4@unca.edu>
2020-10-06 10:45:00 -0400

    QUACK

--- src/main/java/edu/unca/csci/DuckDuckGoController.java
+++ src/main/java/edu/unca/csci/DuckDuckGoController.java
@@ -3,15 +3,4 @@ package edu.unca.csci;
 import javafx.fxml.FXML;
 
 public class DuckDuckGoController {
-<<<<<<< HEAD
-=======
-	public void DuckDuckGoController() {
-
-	}
-
-	@FXML
-	public void initialize() {
-
-	}
->>>>>>> ab3de179d7a89b102f6deb9e94531045fb8b5772
 }
