ab3de179d7a89b102f6deb9e94531045fb8b5772
Ethan Green <egreen4@unca.edu>
2020-10-06 12:40:12 +0000

    Maybe on target for compilation, not sure. Can't tell on my terminal

--- src/main/java/edu/unca/csci/DuckDuckGoController.java
+++ src/main/java/edu/unca/csci/DuckDuckGoController.java
@@ -1,5 +1,14 @@
 package edu.unca.csci;
 
+import javafx.fxml.FXML;
+
 public class DuckDuckGoController {
-    
+	public void DuckDuckGoController() {
+
+	}
+
+	@FXML
+	public void initialize() {
+
+	}
 }
