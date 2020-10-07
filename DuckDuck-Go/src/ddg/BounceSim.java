fe5ddc4abdade7194a12821eb9c08173584bb7a9
Ethan Green <egreen4@unca.edu>
2020-10-06 20:55:16 -0400

    commit prior to merge with Swing primary dev branch

--- DuckDuck-Go/src/ddg/BounceSim.java
+++ DuckDuck-Go/src/ddg/BounceSim.java
@@ -5,14 +5,23 @@ public class BounceSim {
     private final double entityWidth;
     private final double entityHeight;
 
+    private double entitySpeed;
+    private double entityDirection;
+
     public BounceSim(double paneWidth, double paneHeight, double entityWidth, double entityHeight, double decayRate) {
         this.paneWidth = paneWidth;
         this.paneHeight = paneHeight;
 
         this.entityHeight = entityHeight;
         this.entityWidth = entityWidth;
+
+        this.entitySpeed = 0;
+        this.entityDirection = 0;
     }
 
-    public void bump(int direction, double rate) {
+    public void bump(double direction, double speed) {
+    }
+
+    public Position moveNext() {
     }
 }
