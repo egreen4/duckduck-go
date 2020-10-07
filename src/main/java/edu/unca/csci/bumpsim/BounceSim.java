86501c40ba7fce92b46a28ce8da2ae15ec3d5469
Ethan Green <egreen4@unca.edu>
2020-10-07 15:39:48 -0400

    collisions with the floor should work a little bit better

--- src/main/java/edu/unca/csci/bumpsim/BounceSim.java
+++ src/main/java/edu/unca/csci/bumpsim/BounceSim.java
@@ -53,13 +53,13 @@ public class BounceSim {
         }
 
         // If the vertical velocity is very close to 0, set it to 0.
-        if (Math.abs(verticalSpeed) <= .5) {
+        if (Math.abs(verticalSpeed) <= 1) {
             verticalSpeed = 0;
         }
 
         // If the vertical speed is 0, reduce the horizontal speed ("friction").
         if (verticalSpeed == 0) {
-            horizontalSpeed -= 1;
+            horizontalSpeed += 1;
         }
 
         return new Position(xCoordinate, yCoordinate);
